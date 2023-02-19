17a43809468e5d1895d7253ab633d00086cf1eba
deep-patel21 <103757105+deep-patel21@users.noreply.github.com>
2023-02-19 07:56:40 -0500

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-board.analogRead(0, function(value) {
-    let velocity = document.getElementById('speed-value');
-    let tilt = document.getElementById('tilt-value');
-    //
-    velocity.innerHTML = value + " m/s";
-    tilt.innerHTML = tilt + " degrees";
-});
-  
